56136c4345bfaa19b665ba9bb7575dbae33763a9
Vinaykumar-yagati-4 <vinaykumar774474@gmail.com>
2026-08-11 21:55:19 +0530

    feat(meter-management-service): add meter domain model

---
+++ meter-management-service/src/main/java/com/voltaras/metermanagementservice/enums/MeterType.java
@@ -0,0 +1,16 @@
+package com.voltaras.metermanagementservice.enums;
+
+/**
+ * Technology type of an electricity meter.
+ */
+public enum MeterType {
+
+    /** IoT-enabled meter capable of remote reading. */
+    SMART,
+
+    /** Electronic digital meter with a digital display. */
+    DIGITAL,
+
+    /** Conventional electromechanical meter. */
+    ANALOG
+}
---
+++ meter-management-service/src/main/java/com/voltaras/metermanagementservice/enums/PhaseType.java
@@ -0,0 +1,13 @@
+package com.voltaras.metermanagementservice.enums;
+
+/**
+ * Electrical phase configuration of the meter.
+ */
+public enum PhaseType {
+
+    /** Single-phase supply. */
+    SINGLE_PHASE,
+
+    /** Three-phase supply. */
+    THREE_PHASE
+}
